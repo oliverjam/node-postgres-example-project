f9c6493102a863359a297ea52dab930b1dea0392
oliverjam <oliverjamesphillips@gmail.com>
2019-12-15 23:19:01 +0000

    Add comments explaining all error handling

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -4,12 +4,16 @@ const errorView = require("../views/error");
 
 function handleHome(request, response) {
   fs.readFile(__dirname + "/../posts.json", "utf-8", (error, data) => {
+    // if we fail to read the posts respond with an error page
     if (error) {
       console.log(error);
       const html = errorView("Error reading posts data");
       response.writeHead(500, { "content-type": "text/html" });
       response.end(html);
     }
+
+    // if we have posts parse the string into JSON
+    // then respond with the homepage with the posts
     const posts = JSON.parse(data);
     const html = homeView(posts);
     response.writeHead(200, { "content-type": "text/html" });
--- src/handlers/submit.js
+++ src/handlers/submit.js
@@ -3,44 +3,60 @@ const fs = require("fs");
 const errorView = require("../views/error");
 
 function handleSubmit(request, response) {
-  let data = "";
-  request.on("data", chunk => (data += chunk));
+  // read the POST body (it doesn't come all at once)
+  let body = "";
+
+  // whenever we get some data add it to the string
+  request.on("data", chunk => (body += chunk));
+
+  // when we have all the data run this function
   request.on("end", submitError => {
+    // respond with an error if something went wrong with the form submission
     if (submitError) {
       console.log(submitError);
       const html = errorView("Error receiving form data");
       response.writeHead(500, { "content-type": "text/htm;" });
       response.end(html);
     }
-    const newPost = qs.parse(data);
-    fs.readFile(
-      __dirname + "/../posts.json",
-      "utf-8",
-      (readError, postsData) => {
-        if (readError) {
-          console.log(readError);
-          const html = errorView("Error reading posts data");
-          response.writeHead(500, { "content-type": "text/htm;" });
-          response.end(html);
-        }
-        const posts = JSON.parse(postsData);
-        posts.push(newPost);
-        fs.writeFile(
-          __dirname + "/../posts.json",
-          JSON.stringify(posts),
-          writeError => {
-            if (writeError) {
-              console.log(writeError);
-              const html = errorView("Error saving post data");
-              response.writeHead(500, { "content-type": "text/htm;" });
-              response.end(html);
-            }
-            response.writeHead(302, { Location: "/" });
-            response.end();
-          }
-        );
+
+    // turn the form submitted data into an object
+    const newPost = qs.parse(body);
+
+    // read the file where posts are stored
+    fs.readFile(__dirname + "/../posts.json", "utf-8", (readError, file) => {
+      // if we fail to read the file respond with an error
+      if (readError) {
+        console.log(readError);
+        const html = errorView("Error reading posts data");
+        response.writeHead(500, { "content-type": "text/htm;" });
+        response.end(html);
       }
-    );
+
+      // turn the file contents (string) into JSON (array)
+      const posts = JSON.parse(file);
+
+      // put the newly submitted post into the array
+      posts.push(newPost);
+
+      // overwrite the old posts file with the updated posts
+      fs.writeFile(
+        __dirname + "/../posts.json",
+        JSON.stringify(posts),
+        writeError => {
+          // if we fail to save the posts respond with an error
+          if (writeError) {
+            console.log(writeError);
+            const html = errorView("Error saving post data");
+            response.writeHead(500, { "content-type": "text/html" });
+            response.end(html);
+          }
+
+          // if everything was successful redirect to homepage
+          response.writeHead(302, { Location: "/" });
+          response.end();
+        }
+      );
+    });
   });
 }
 
