c84db1dea5e87c9e26eeee851db4cea124036a35
oliverjam <oliverjamesphillips@gmail.com>
2019-12-15 22:51:12 +0000

    Support submitting new posts

--- src/handlers/submit.js
+++ src/handlers/submit.js
@@ -1,4 +1,5 @@
 const qs = require("querystring");
+const fs = require("fs");
 
 function handleSubmit(request, response) {
   let data = "";
@@ -9,8 +10,30 @@ function handleSubmit(request, response) {
       response.end(`<h1>Oops</h1>`);
     }
     const newPost = qs.parse(data);
-    response.statusCode = 200;
-    response.end(`<pre>${JSON.stringify(newPost, null, 2)}</pre>`);
+    fs.readFile(
+      __dirname + "/../posts.json",
+      "utf-8",
+      (readError, postsData) => {
+        if (readError) {
+          console.log(readError);
+          response.statusCode = 500;
+          response.end(`<h1>Oops</h1>`);
+        }
+        const posts = JSON.parse(postsData);
+        posts.push(newPost);
+        fs.writeFile(
+          __dirname + "/../posts.json",
+          JSON.stringify(posts),
+          writeError => {
+            if (writeError) {
+              console.log(writeError);
+            }
+            response.writeHead(302, { Location: "/" });
+            response.end();
+          }
+        );
+      }
+    );
   });
 }
 
