4987f91933c5fb4362ab7c4c8d32c82e9ef1541f
oliverjam <oliverjamesphillips@gmail.com>
2019-12-15 20:12:46 +0000

    Add url-based routing and 404 page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
 const html = require("./html");
 
 function router(request, response) {
-  response.statusCode = 200;
-  response.end(html(`<h1>Hello world</h1>`));
+  switch (request.url) {
+    case "/":
+      response.statusCode = 200;
+      response.end(html(`<h1>Hello world</h1>`));
+      break;
+    default:
+      response.statusCode = 404;
+      response.end(html(`<h1>Page not found</h1>`));
+  }
 }
 
 module.exports = router;
