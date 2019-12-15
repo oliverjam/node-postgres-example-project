c95fc77008459a6128fc435c9364a3e4352db663
oliverjam <oliverjamesphillips@gmail.com>
2019-12-15 23:44:41 +0000

    Add some basic styles

--- src/pageTemplate.js
+++ src/pageTemplate.js
@@ -6,6 +6,7 @@ function pageTemplate(body) {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Node project</title>
+        <link rel="stylesheet" href="/assets/styles.css">
       </head>
       <body>
         ${body}
--- src/views/home.js
+++ src/views/home.js
@@ -4,26 +4,27 @@ function homeView(posts) {
   return html(`
   <h1>Blogr</h1>
   <form action="/submit" method="POST">
+    <h2>New post</h2>
     <label for="author">Your name</label>
     <input id="author" name="author" required />
     <label for="title">Post title</label>
     <input id="title" name="title" required />
     <label for="body">Post</label>
-    <textarea id="body" name="body" required></textarea>
+    <textarea id="body" name="body" rows="6" required></textarea>
     <button>Submit</button>
   </form>
   <h2>Posts</h2>
-  <ul>
+  <ul class="posts">
   ${posts
     .map(
       post => `<li>
-  <h3>${post.title}</h3>
-  <p>${post.author}</p>
-  </li>`
+      <h3>${post.title}</h3>
+      <p>${post.author}</p>
+      </li>`
     )
     .join("")}
-  </ul>
-`);
+      </ul>
+  `);
 }
 
 module.exports = homeView;
