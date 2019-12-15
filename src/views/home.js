065442a07d3d47aca00c9dde3082bc8de3f11f7f
oliverjam <oliverjamesphillips@gmail.com>
2019-12-15 23:19:07 +0000

    Make form inputs required

--- src/views/home.js
+++ src/views/home.js
@@ -5,11 +5,11 @@ function homeView(posts) {
   <h1>Blogr</h1>
   <form action="/submit" method="POST">
     <label for="author">Your name</label>
-    <input id="author" name="author" />
+    <input id="author" name="author" required />
     <label for="title">Post title</label>
-    <input id="title" name="title" />
+    <input id="title" name="title" required />
     <label for="body">Post</label>
-    <textarea id="body" name="body"></textarea>
+    <textarea id="body" name="body" required></textarea>
     <button>Submit</button>
   </form>
   <h2>Posts</h2>
